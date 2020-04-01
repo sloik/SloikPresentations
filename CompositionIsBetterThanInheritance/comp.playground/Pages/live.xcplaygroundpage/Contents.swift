6c06f96e7e125e5e3722031855c5e0abe1b47e81
Lukasz Stocki <stocki.lukasz@gmail.com>
2020-04-01 20:51:41 +0200

    Adds VP Composition over Inheritance live demo playground.
    Adds readme.

---
+++ CompositionIsBetterThanInheritance/comp.playground/Pages/live.xcplaygroundpage/Contents.swift
@@ -0,0 +1,64 @@
+import UIKit
+
+/*:
+ # DISCALEMER
+ 
+ I stand here on the shoulders of giants and here are those giants that inspired me to put it all in one place. Please check their work and be inspired as I am ✌🏻
+ 
+ * [pointfree.co - there I saw a lot of operators that I show here](https://www.pointfree.co)
+ * [Runes - frameworks with more operators](https://github.com/thoughtbot/Runes)
+ * [Overture - library for composing functions with ease](https://github.com/pointfreeco/swift-overture)
+  * [F# for fun and profit  -- tons of ideas how to apply this concepts in real life](https://fsharpforfunandprofit.com/video/)
+ 
+ ---
+  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("🥳 👑🦠")
+
+
