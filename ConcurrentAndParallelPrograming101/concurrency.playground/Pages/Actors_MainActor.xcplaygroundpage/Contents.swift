1064a4c3a1e461aad722e7ab145352007473375c
Edward Maliniak <mail@mail.com>
2022-05-26 23:37:20 +0200

    Adds link.

--- ConcurrentAndParallelPrograming101/concurrency.playground/Pages/Actors_MainActor.xcplaygroundpage/Contents.swift
+++ ConcurrentAndParallelPrograming101/concurrency.playground/Pages/Actors_MainActor.xcplaygroundpage/Contents.swift
@@ -1,5 +1,14 @@
 //: [Previous](@previous)
 
-// https://oleb.net/2022/how-mainactor-works/
+
+
+/*:
+
+# Links
+
+ * [How main actor works](https://oleb.net/2022/how-mainactor-works/)
+ * [How do Actors work in Swift?](https://youtu.be/8jvtHCXJ4Ow)
+
+ */
 
 //: [Next](@next)
