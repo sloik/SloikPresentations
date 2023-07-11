15a97d97e5d62fff831905d703aafb2fd1cb66fc
Lukasz Stocki <stocki.lukasz@gmail.com>
2023-07-11 23:10:49 +0200

    Adds new page.

---
+++ ConcurrentAndParallelPrograming101/concurrency.playground/Pages/SC-04-Task_Local_Values.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+/*:
+
+ # [Task Local Values](https://github.com/apple/swift-evolution/blob/main/proposals/0311-task-locals.md)
+
+ # Links
+
+ * [Proposal: SE-031 - Task Local Values](https://github.com/apple/swift-evolution/blob/main/proposals/0311-task-locals.md)
+
+ */
+
+//: [Next](@next)
